aa49307579da07486f77da1278095c70c85ac11a
abhinavbhashyam <84284109+abhinavbhashyam@users.noreply.github.com>
2023-07-24 10:10:47 -0500

    config, initial commit

---
+++ springboot-backend/volsync-project/src/main/java/com/volsync/volsyncproject/VolsyncProjectApplication.java
@@ -0,0 +1,13 @@
+package com.volsync.volsyncproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolsyncProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolsyncProjectApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/volsync-project/src/test/java/com/volsync/volsyncproject/VolsyncProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volsync.volsyncproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolsyncProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
